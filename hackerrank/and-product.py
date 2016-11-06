05167faf5c85b6f2a96097caa20297215981a3bb
Konstantin <vorobev@gmail.com>
2016-11-06 20:01:02 +0900

    Create and-product.py

---
+++ hackerrank/and-product.py
@@ -0,0 +1,31 @@
+import math
+
+def and_sum(a, b):
+    if a == 0:
+        return 0
+    la = int(math.log(a, 2))
+    lb = int(math.log(b, 2))
+    if lb != la:
+        return 0
+    r = 0
+    for p in range(la, 0, -1):
+        ps = 1 << p
+        if (ps & a) ^ (ps & b) == 0:
+            # Both have
+            r = r | (ps & a)
+        else:
+            return r
+    return a
+    r = a
+    i = a + 1
+    while i <= b:
+        r = r & i
+        if r == 0:
+            return r
+        i += 1
+    return r
+
+q = int(raw_input().strip())
+for i in range(q):
+    a, b = map(int, raw_input().strip().split(' '))
+    print and_sum(a, b)
